1a81b26907f403c72e6f133772160c09cea9432f
Michael Gundlach <gundlach@gmail.com>
2022-09-15 18:24:44 -0400

    Sound is a nightmare in Flutter.  3 working sounds.  Good enough.

--- lib/util/sound.dart
+++ lib/util/sound.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:just_audio/just_audio.dart';
 
 class Sound {
@@ -6,18 +8,25 @@ class Sound {
   static final _sound = Sound._();
 
   static Future<void> play(Sounds sound) async {
-    await _sound._player.setAsset('assets/audio/${_sound._fileForType(sound)}');
-    await _sound._player.play();
+    // TODO Convince Flutter they have a bug re assets/ - it will randomly want to find them with or
+    // without the assets/ prefix! >:(
+    try {
+      await _sound._player.setAsset('assets/audio/${_sound._fileForType(sound)}');
+    } catch (e) {
+      await _sound._player.setAsset('audio/${_sound._fileForType(sound)}');
+    }
+    _sound._player.play();
   }
 
   String _fileForType(Sounds sound) {
+    // TODO find out why some sounds work and some don't. What is wrong with their encoding?
     switch (sound) {
       case Sounds.highlighted:
-        return "tap.mp3";
+        return "card-switch.wav";
       case Sounds.played:
-        return "tap2.mp3";
+        return "card1.wav";
       case Sounds.failed:
-        return "tap3.mp3";
+        return "cardSlide7.wav";
     }
   }
 }
