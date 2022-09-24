110fa922f28fddcdb42804240937a9ec83d68915
Michael Gundlach <gundlach@gmail.com>
2022-09-24 14:31:01 -0400

    animate More button

--- lib/views/free-spaces.dart
+++ lib/views/free-spaces.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freecell/main.dart';
@@ -23,15 +22,14 @@ class FreeSpaces extends ConsumerWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            if (gameState.freeCellsAreFull) _moreButton(context, gameState, cardWidth),
+            _moreButton(context, gameState, cardWidth),
             ...gameState.freeCells.map(
               (pile) => PileView(
-                // TODO right
                 entries: pile,
                 canHighlight: (PileEntry entry) => !entry.isTheBase,
                 canReceive: (PileEntry highlighted, PileEntry entry) => entry.isTheBase,
-                baseBuilder: () => _buildBase(ref, cardWidth, context),
-                positioner: (_, Widget child) => Align(child: child),
+                baseBuilder: () => _buildBase(context, ref, cardWidth),
+                positioner: (_, Widget child) => child,
               ),
             ),
           ],
@@ -40,32 +38,39 @@ class FreeSpaces extends ConsumerWidget {
     });
   }
 
-  Widget _moreButton(context, gameState, cardWidth) {
-    final color = Theme.of(context).highlightColor;
+  Widget _moreButton(context, GameState gameState, cardWidth) {
+    Widget button = const SizedBox.shrink();
+    if (gameState.freeCellsAreFull) {
+      button = GestureDetector(
+        onTap: () => gameState.addFreeCell(),
+        child: Container(
+          width: cardWidth / 1.9,
+          height: cardWidth / 1.9,
+          decoration: BoxDecoration(color: Theme.of(context).highlightColor, borderRadius: BorderRadius.circular(100)),
+          child: FittedBox(fit: BoxFit.contain, child: Text("+", style: Theme.of(context).textTheme.caption)),
+        ),
+      );
+    }
+
     return Center(
       child: Container(
         padding: const EdgeInsets.all(4.0),
         width: cardWidth,
         child: Align(
           alignment: Alignment.centerRight,
-          child: GestureDetector(
-            onTap: () => gameState.addFreeCell(),
-            child: Container(
-              width: cardWidth / 1.9,
-              height: cardWidth / 1.9,
-              decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(100)),
-              child: FittedBox(
-                fit: BoxFit.contain,
-                child: Text("+", style: Theme.of(context).textTheme.caption),
-              ),
-            ),
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 600),
+            reverseDuration: const Duration(milliseconds: 300),
+            switchInCurve: Curves.fastOutSlowIn,
+            switchOutCurve: Curves.fastOutSlowIn,
+            child: button,
           ),
         ),
       ),
     );
   }
 
-  Widget _buildBase(WidgetRef ref, double cardWidth, BuildContext context) {
+  Widget _buildBase(BuildContext context, WidgetRef ref, double cardWidth) {
     return Container(
       width: cardWidth,
       padding: const EdgeInsets.all(4.0), // same padding as PlayingCardView
@@ -81,10 +86,10 @@ class FreeSpaces extends ConsumerWidget {
               transform: Matrix4.rotationZ(-.5),
               alignment: FractionalOffset.center,
               child: FractionallySizedBox(
-                  widthFactor: .7,
-                  heightFactor: .7,
-                  child: FittedBox(
-                      fit: BoxFit.contain, child: Text("FREE", style: Theme.of(context).textTheme.headline4))),
+                widthFactor: .7,
+                heightFactor: .7,
+                child: FittedBox(fit: BoxFit.contain, child: Text("FREE", style: Theme.of(context).textTheme.caption)),
+              ),
             ),
           ),
         ),
